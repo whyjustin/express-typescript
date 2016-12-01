fe05b1498ada351eba5f34133f7f1c2780e1745c
Justin Young <jyoung@sonatype.com>
2016-12-01 13:13:11 -0800

    Include source maps with compilation. Guess there is never a last commit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
-  const tsResult = tsProject.src().pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest('www'));
+gulp.task('compile', () => {
+  const tsResult = tsProject
+    .src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject());
+  return tsResult.js
+    .pipe(sourcemaps.write('.', {sourceRoot: '/src'}))
+    .pipe(gulp.dest('www'));
 });
 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/**/*.ts', ['scripts']);
+gulp.task('watch', ['compile'], () => {
+  gulp.watch('src/**/*.ts', ['compile']);
 });
 
 gulp.task('default', ['watch']);
